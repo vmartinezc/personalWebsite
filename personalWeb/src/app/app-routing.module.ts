02a03a13867a6a4c6ef3ad47eabf5e263e44476d
valeria martinez <vamc98@hotmail.com>
2021-03-11 12:45:11 -0500

    Made a couple of components & fixed some bugs

--- personalWeb/src/app/app-routing.module.ts
+++ personalWeb/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutMeComponent} from './pages/about-me/about-me.component';
+// import { PortfolioComponent} from './pages/portfolio/portfolio.component';
+// import { ResumeComponent} from './pages/resume/resume.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path: 'aboutme', component: AboutMeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- personalWeb/src/app/app.module.ts
+++ personalWeb/src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutMeComponent } from './about-me/about-me.component';
-import { ResumeComponent } from './resume/resume.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { ResumeComponent } from './pages/resume/resume.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutMeComponent,
     ResumeComponent,
-    PortfolioComponent
+    PortfolioComponent,
+    MenuBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
